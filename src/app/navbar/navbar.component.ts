0d37a7f5596028054c280f35b6c918285f56e38e
josepereza <josepereza66@gmail.com>
2020-04-11 06:35:38 +0200

    modificacion menu

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  public micondition: boolean = true;
 
+  @HostListener('window:scroll') scrolling() {
+    console.log('scrolling');
+    if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
+      this.micondition = false;
+    } else this.micondition = true;
+  }
   constructor() { }
 
   ngOnInit(): void {
+
+
+
   }
 
 }
